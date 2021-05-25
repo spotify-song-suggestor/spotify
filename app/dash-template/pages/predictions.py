d29aa7deffafbae7fe3d5c96cd6e883e1493b143
LoganTilley <logan.e.tilley@gmail.com>
2021-05-25 18:31:42 -0400

    Moved some files for convenience, added model and encoded data. Wrote recommend function

--- app/dash-template/pages/predictions.py
+++ app/dash-template/pages/predictions.py
@@ -4,22 +4,40 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+import joblib
+from tensorflow.keras.models import load_model
+import numpy as np
 # Imports from this application
 from app import app
+
+
+encodings = joblib.load(r'app\dash-template\assets\encoded_data.joblib')
+knn = joblib.load(r'app\dash-template\assets\knn.joblib')
+model = load_model(r'app\dash-template\assets\ae4')
+
+
+def recommend(index: int, n: int=5):
+    '''
+    index: index of song
+    n: number of recommendations to pull
+    returns: array of distances, array of indexes for recommended songs. Includes
+        original song.
+    '''
+    return knn.kneighbors([encodings[index]], n_neighbors=5)
+
+
+def get_songs(indeces: 'list[int]'):
+    ''' TODO
+    Uses SQLAlchemy queries to return track data from their indeces
+    '''
+    return None
+
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
         dcc.Markdown(
-            """
-        
-            ## Predictions
-
-            Your instructions: How to use your app to get new predictions.
-
-            """
         ),
     ],
     md=4,
